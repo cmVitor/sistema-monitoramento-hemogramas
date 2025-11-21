e4f3b00a4d94305c32b0f26e156ccb5a5a2a0d92
Leonardo Ribeiro <leonardo25palmeira250@gmail.com>
2025-11-21 02:34:35 -0300

    View para alertas

---
+++ application/src/views/__init__.py
@@ -0,0 +1,16 @@
+"""
+Views package for organizing API endpoints.
+"""
+from .health import router as health_router
+from .observations import router as observations_router
+from .alerts import router as alerts_router
+from .heatmap import router as heatmap_router
+from .data_generation import router as data_generation_router
+
+__all__ = [
+    "health_router",
+    "observations_router",
+    "alerts_router",
+    "heatmap_router",
+    "data_generation_router",
+]
